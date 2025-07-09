ac50088d875dcbab4acd838f4e67b20c5f462c4f
maysasfour <mays.suhail@gmail.com>
2025-07-09 21:34:56 +0300

    Factory Method Design Pattern

---
+++ src/A12June/FactoryMethod/Pizza.java
@@ -0,0 +1,11 @@
+package A12June.FactoryMethod;
+
+public abstract  class Pizza {
+    public abstract void prepere();
+    public void bake(){System.out.println("T : 180 ");}
+    public void cut(){System.out.println("Cut");}
+    public void box(){System.out.println("box");}
+
+
+
+}
